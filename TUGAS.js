2b979cf0024d05517a62504d16ecd3e45fbf689d
Chikara Maestro <chikaramaestro20@gmail.com>
2025-12-12 21:17:26 +0700

    feat : Money Logic

--- TUGAS.js
+++ TUGAS.js
@@ -1,3 +1,5 @@
+// TUGAS 1 Front-end (Chikara Maestro Inozaki_102022500246)
+
 const input= require('prompt-sync')({sigint: true})
 const nama = input("Siapa Namamu: ")
 const umur = Number(input("Masukan umur: "))
@@ -5,7 +7,15 @@ const umur = Number(input("Masukan umur: "))
 
 if(umur>=21)
 {
-    
+    const uang = Number(input("Masukan Uang: "))
+    if(uang>=500000)
+    {
+        console.log(`Selamat Datang ${nama}`);
+    }
+    else
+    {
+        console.log(`Maaf ${nama}, Uang kamu cuma ${uang}, Datang lagi lain kali`)
+    }
 }
 else
 {
