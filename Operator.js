b8a4b9323bbd6dc45a8170b41f29c1e84dc953a3
Chikara Maestro <chikaramaestro20@gmail.com>
2025-12-12 20:52:11 +0700

    chore : init project

--- Operator.js
+++ Operator.js
@@ -1,5 +1,5 @@
-const a = 10
-const b = 4
+const a = 4
+const b = "4"
 
 console.log(a + b)
 console.log(a - b)
@@ -14,3 +14,5 @@ console.log( a > b)
 console.log(a >= b)
 console.log(a !== b)// MENGECEK APAKAH KEDUA BILANGAN BERBEDA
 console.log(a === b)
+
+console.log(a == b)//menyamakan tipe data
---
+++ TUGAS.js
@@ -0,0 +1,5 @@
+const input= require('prompt-sync')({sigint: true})
+const nama = input("Siapa Namamu: ")
+const umur = Number(input("Masukan umur: "))
+
+
